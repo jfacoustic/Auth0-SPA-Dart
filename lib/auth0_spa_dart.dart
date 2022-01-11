885004ec6a3b5cbb95f4d9312fc6afd04b28ba61
Joshua Mathews <josh@32.team>
2022-01-11 15:12:51 -0700

    Initial Commit

---
+++ lib/auth0_spa_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library auth0_spa_dart;
+
+export 'src/auth0_spa_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
