1aac99cf1b84f797beee85db0b2777b417826a2e
Joshua Mathews <josh@32.team>
2022-01-13 09:51:06 -0700

    Update example.

--- example/web/main.dart
+++ example/web/main.dart
@@ -1,5 +1,30 @@
 import 'dart:html';
 
-void main() {
+import 'package:auth0_spa_dart/auth0_spa_dart.dart';
+import 'package:js/js.dart';
+
+const clientId = '{YOUR AUTH0 CLIENT ID}';
+const domain = '{YOUR AUTH0 DOMAIN}';
+
+@JS('loginWithPopup')
+external set _loginWithPopup(void Function() f);
+
+@JS()
+external void loginWithPopup();
+
+@JS('getTokenSilently')
+external set _getTokenSilently(void Function() f);
+
+@JS()
+external void getTokenSilently();
+
+void main() async {
   querySelector('#output')?.text = 'Your Dart app is running.';
+  final client = await createAuth0Client(clientId: clientId, domain: domain);
+
+  _loginWithPopup = allowInterop(client.loginWithPopup);
+  _getTokenSilently = allowInterop(() async {
+    final token = await client.getTokenSilently();
+    querySelector('#output')?.text = token;
+  });
 }
